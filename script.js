87e51f95579fb50ed150ee07e897a4e75751aea7
pdr606 <119369768+pdr606@users.noreply.github.com>
2023-01-31 19:42:57 -0300

    final-project

--- script.js
+++ script.js
@@ -5,38 +5,45 @@ function verificar(){
     var ano = data.getFullYear()
     var res = document.getElementById('res')
 
-    if (yearnasc.value.lenght == 0  || yearnasc.value > ano){
+    if (yearnasc.value.length == 0 && yasnasc.value > ano ){
         window.alert('[ERROR] Check dados and try again')
     } else {
-        var sexo = document.getElementsByName('sex')
+        var fsex = document.getElementsByName('sex')
         var idade = ano - Number(yearnasc.value)
         var genero = ''
         var img = document.getElementById('foto')
-        if (sexo[0].checked){
-            genero = 'Homem'
+        } if(fsex[0].checked){
+            genero = 'Masculino'
             if (idade >= 0 && idade < 10){
                 img.src = 'foto-bebe-m.png'
-            } else if ( idade < 21) {
+            } else if (idade < 21){
                 img.src = 'foto-jovem-m.png'
-            } else if ( idade < 51) {   
+            } else if (idade < 51){
                 img.src = 'foto-adulto-m.png'
             } else{
                 img.src = 'foto-idoso-m.png'
             }
-
-        } else if (sexo[1].checked)
-            genero = 'Mulher'
+        } else if (fsex[1].checked){
+            genero = 'Feminino'
             if (idade >= 0 && idade < 10){
                 img.src = 'foto-bebe-f.png'
-            } else if ( idade < 21) {
+            } else if (idade < 21){
                 img.src = 'foto-jovem-f.png'
-            } else if ( idade < 51) {
-                img.src = 'foto-jovem-f.png'
+            } else if (idade < 51){
+                img.src = 'foto-adulto-f.png'
             } else{
                 img.src = 'foto-idoso-f.png'
             }
 
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        
+            
+        }
+
+        res.innerHTML = `Detectamos genero ${genero} com ${idade} anos `
+
+        
+     
     }
 
-}
+    
+
